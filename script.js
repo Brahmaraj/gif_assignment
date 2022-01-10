355334bbaa9232dfde8ee358b227ea0d5538afb3
Brahmaraj <brahmarajrathod@gmail.com>
2022-01-10 09:14:38 +0530

    load more

--- script.js
+++ script.js
@@ -8,7 +8,14 @@ function loadGif(){
             console.log(data)
             let count=1;
             data.data.forEach(element => {
+                var img_tag = document.createElement('img');
+                img_tag.id = "addgif"+count;
+                img_tag.className = "common";
+                //console.log(img_tag);
+                document.getElementById('main-container').appendChild(img_tag);
                 document.getElementById("addgif"+count).src = element.images.downsized.url;
+                img_tag.src = element.images.downsized.url;
+                //document.appendChild
                 count++;
             });
             //document.getElementById('addgif1').src = data.data[0].images.downsized.url;
